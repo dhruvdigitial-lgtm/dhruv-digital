07f833a255136662be491d19b7978fa7e72b7e03
Dhruv Yadav <dhruvdigitial@gmail.com>
2026-06-30 09:30:13 +0530

    fix sitemap content type

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,23 @@
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
-
   allowedDevOrigins: [
     "192.168.7.9"
-  ]
+  ],
 
+  async headers() {
+    return [
+      {
+        source: "/sitemap.xml",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/xml",
+          },
+        ],
+      },
+    ];
+  },
 };
-
 
 export default nextConfig;
